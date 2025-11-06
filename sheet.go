79e0e1e0f0447ffd4fee13e6bbcfdcdb4dd09a26
Today <76273733+today2098@users.noreply.github.com>
2025-11-06 21:23:24 +0000

    [clean] Create base sheet type shared by Sheet and SheetWithStreamWriter

--- sheet.go
+++ sheet.go
@@ -1,67 +1,20 @@
 package exceltable
 
 import (
-	"fmt"
 	"reflect"
-	"strings"
-
-	"github.com/xuri/excelize/v2"
 )
-
-const (
-	csvTag   string = "csv"
-	excelTag string = "excel"
-
-	defaultTableStyle = "TableStyleMedium6"
-)
-
-type sheetRule struct {
-	predKey predKeyType
-	styleID int
-}
 
 type Sheet[M any] struct {
-	File            *File
-	name            string
-	x, y            int
-	row, tableWidth int
-	numField        int
-	skip            []bool
-	header          []any
-	rulesList       [][]*sheetRule
+	sheetBase[M]
 }
 
 func NewSheet[M any](f *File, name, cell string, active bool) (*Sheet[M], error) {
-	idx, err := f.File.NewSheet(name)
-	if err != nil {
-		return nil, err
-	}
-	if active {
-		f.File.SetActiveSheet(idx)
-	}
-
-	x, y, err := excelize.CellNameToCoordinates(cell)
-	if err != nil {
+	s := &Sheet[M]{}
+	if err := s.construct(f, name, cell, active); err != nil {
 		return nil, err
 	}
 
-	tableWidth, numFiled, skip, header, rulesList, err := getFieldValue(reflect.TypeFor[M](), f.rules)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Sheet[M]{
-		File:       f,
-		name:       name,
-		x:          x,
-		y:          y,
-		row:        0,
-		tableWidth: tableWidth,
-		numField:   numFiled,
-		skip:       skip,
-		header:     header,
-		rulesList:  rulesList,
-	}, nil
+	return s, nil
 }
 
 func (s *Sheet[M]) SetHeader() error {
@@ -74,8 +27,6 @@ func (s *Sheet[M]) SetHeader() error {
 }
 
 func (s *Sheet[M]) SetRow(obj *M) error {
-	s.row++
-
 	ptrV := reflect.ValueOf(obj)
 	v := ptrV.Elem()
 
@@ -108,6 +59,7 @@ func (s *Sheet[M]) SetRow(obj *M) error {
 
 		col++
 	}
+	s.row++
 
 	return nil
 }
@@ -127,66 +79,4 @@ func (s *Sheet[M]) AddDefaultTable() error {
 
 func (s *Sheet[M]) AddTable(styleName string) error {
 	return s.File.File.AddTable(s.name, s.newTable(styleName))
-}
-
-func (s *Sheet[M]) newTable(styleName string) *excelize.Table {
-	topLeftCell := s.coordinatesToCellName(0, 0)
-	bottomRightCell := s.coordinatesToCellName(max(s.tableWidth-1, 1), max(s.row, 1))
-	return &excelize.Table{
-		Range:     fmt.Sprintf("%s:%s", topLeftCell, bottomRightCell),
-		Name:      fmt.Sprintf("%sTable", s.name),
-		StyleName: styleName,
-	}
-}
-
-func (s *Sheet[M]) coordinatesToCellName(col, row int) string {
-	cell, err := excelize.CoordinatesToCellName(s.x+col, s.y+row)
-	if err != nil {
-		panic(err)
-	}
-	return cell
-}
-
-func getFieldValue(t reflect.Type, fileRules []*fileRule) (tableWidth, numFiled int, skip []bool, header []any, rulesList [][]*sheetRule, err error) {
-	if t.Kind() != reflect.Struct {
-		return 0, 0, nil, nil, nil, ErrNotStructType
-	}
-
-	tableWidth, numFiled = 0, t.NumField()
-	skip = make([]bool, numFiled)
-	header = make([]any, 0, numFiled)
-	rulesList = make([][]*sheetRule, 0, numFiled)
-	for i := range numFiled {
-		field := t.Field(i)
-		if field.PkgPath != "" { // field is unexported.
-			skip[i] = true
-			continue
-		}
-
-		h := field.Tag.Get(excelTag)
-		if h == "" {
-			h = field.Tag.Get(csvTag)
-		}
-
-		switch h {
-		case "":
-			h = field.Name
-		case "-":
-			skip[i] = true
-			continue
-		}
-
-		header = append(header, h)
-		tableWidth++
-
-		rules := make([]*sheetRule, 0)
-		for _, fileRule := range fileRules {
-			for key := range strings.SplitSeq(field.Tag.Get(fileRule.tag), ",") {
-				rules = append(rules, &sheetRule{key, fileRule.styleID})
-			}
-		}
-		rulesList = append(rulesList, rules)
-	}
-
-	return
 }
