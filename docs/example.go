c22d61fb16dcf92154c51e7d71b8939b1314e1b9
Today <76273733+today2098@users.noreply.github.com>
2025-11-07 12:10:12 +0000

    [add] Add description to README

---
+++ docs/example.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"slices"
+
+	"github.com/today2098/exceltable"
+	"github.com/xuri/excelize/v2"
+)
+
+type Person struct {
+	ID            string  `error:"zero"`
+	Name          string  `csv:"name" excel:"氏名" newface:"isNewFace" error:"zero"`
+	Age           int     `csv:"age" excel:"年齢" warn:"IsChild,IsOld"`
+	Address       string  `csv:"address" excel:"住所" warn:"-"`
+	AccountNumber string  `csv:"account_number" excel:"-"`
+	SpecialID     *string `warn:"notZero" error:"nil"`
+}
+
+func (p *Person) IsChild() bool {
+	return p.Age < 18
+}
+
+func (p Person) IsOld() bool {
+	return 75 <= p.Age
+}
+
+func init() {
+	exceltable.RegisterRule(0, "newface", &excelize.Style{
+		Fill: excelize.Fill{
+			Type:    "pattern",
+			Pattern: 1,
+			Color:   []string{"#aaffaa"},
+		},
+	})
+
+	exceltable.RegisterPredicate("isNewFace", func(name string) bool {
+		newFaces := []string{"Alice"}
+		return slices.Contains(newFaces, name)
+	})
+}
+
+func main() {
+	aliceSpecialID := ""
+	alice := &Person{
+		ID:            "ID-123456",
+		Name:          "Alice",
+		Age:           17,
+		Address:       "",
+		AccountNumber: "0000-0000-0000-0000",
+		SpecialID:     &aliceSpecialID,
+	}
+
+	bob := &Person{
+		ID:            "ID-112358",
+		Name:          "Bob",
+		Age:           32,
+		Address:       "Boston",
+		AccountNumber: "1111-1111-1111-1111",
+		SpecialID:     nil,
+	}
+
+	carolSpecialID := "SID-999999"
+	carol := &Person{
+		ID:            "",
+		Name:          "Carol",
+		Age:           100,
+		Address:       "京都",
+		AccountNumber: "",
+		SpecialID:     &carolSpecialID,
+	}
+
+	f, _ := exceltable.NewFile()
+
+	s, _ := exceltable.NewSheetWithStreamWriter[Person](f, "People", "A1", true)
+	s.SetHeader()
+
+	s.SetRow(alice)
+	s.SetRow(bob)
+	s.SetRow(carol)
+
+	s.AddDefaultTable()
+	s.Flush()
+
+	f.SaveAs("people.xlsx")
+}
